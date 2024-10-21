db68a0d8015a756080b3e526d6d5c4ecf480369a
cremyk <yingkit0212@gmail.com>
2024-10-21 23:16:43 +0800

    update

---
+++ Lab7/task7.2.js
@@ -0,0 +1,44 @@
+var dataset = [5, 10, 20, 45, 6, 25]; //simple dataset
+var pie = d3.pie(); //define default pie
+
+var w = 300;
+var h = 300;
+
+var outerRadius = w / 2;
+var innerRadius = 0;
+var arc = d3.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(outerRadius);
+
+//create svg element
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+//set up groups
+var arcs = svg.selectAll("g.arc")
+                .data(pie(dataset))
+                .enter()
+                .append("g")
+                .attr("class", "arc")
+                .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")");
+
+//draw arcpaths
+arcs.append("path")
+    .attr("fill", function(d, i) {
+        return arc(d, i);
+    });
+
+//colour scale
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+//text label for each wedge
+arcs.append("text")
+    .attr("transform", function(d) {
+        return "translate("  + arc.centroid(d) + ")";
+    })
+    .attr("text-anchor", "middle")
+    .text(function(d) {
+        return getComputedStyle.value;
+    });
