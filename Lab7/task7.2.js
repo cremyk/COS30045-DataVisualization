1048d9f8d5f0dc866045b74c02ebcc9710923e63
cremyk <yingkit0212@gmail.com>
2024-10-21 23:32:39 +0800

    Update task7.2.js

--- Lab7/task7.2.js
+++ Lab7/task7.2.js
@@ -1,5 +1,7 @@
-var dataset = [5, 10, 20, 45, 6, 25]; //simple dataset
-var pie = d3.pie(); //define default pie
+var dataset = [5, 10, 20, 45, 6, 25]; // Simple dataset
+
+// Define the pie layout
+var pie = d3.pie(); // Creates a pie layout generator
 
 var w = 300;
 var h = 300;
@@ -10,36 +12,36 @@ var arc = d3.arc()
             .innerRadius(innerRadius)
             .outerRadius(outerRadius);
 
-//colour scale
+// Define the color scale
 var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-//create svg element
+// Create the SVG element
 var svg = d3.select("body")
             .append("svg")
             .attr("width", w)
             .attr("height", h);
 
-//set up groups
+// Set up groups for the arcs
 var arcs = svg.selectAll("g.arc")
-                .data(pie(dataset))
+                .data(pie(dataset)) // Bind the pie data to the arcs
                 .enter()
                 .append("g")
                 .attr("class", "arc")
-                .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")");
+                .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")"); // Center the pie chart
 
-//draw arcpaths
+// Draw arc paths
 arcs.append("path")
     .attr("fill", function(d, i) {
-        return color(i);
+        return color(i); // Use color scale for different segments
     })
-    .attr("d", arc); //generate arc shapes
+    .attr("d", arc); // Generate the arc shapes
 
-//text label for each wedge
+// Add text labels for each wedge
 arcs.append("text")
     .attr("transform", function(d) {
-        return "translate("  + arc.centroid(d) + ")";
+        return "translate(" + arc.centroid(d) + ")"; // Position the text in the center of each wedge
     })
     .attr("text-anchor", "middle")
     .text(function(d) {
-        return d.data;
+        return d.value; // Display the value of each slice (this comes from the pie layout)
     });
