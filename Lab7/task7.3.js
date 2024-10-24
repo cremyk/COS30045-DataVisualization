69591ab0a724de11d285ffeecaaa60e06a7089ff
cremyk <yingkit0212@gmail.com>
2024-10-24 13:23:36 +0800

    update

--- Lab7/task7.3.js
+++ Lab7/task7.3.js
@@ -1,99 +1,76 @@
-var dataset = [
-    { apples: 5, oranges: 10, grapes: 22}, //blue =apples
-    { apples: 4, oranges: 12, grapes: 28}, //orange =oranges
-    { apples: 2, oranges: 19, grapes: 32}, //green =grapes
-    { apples: 7, oranges: 23, grapes: 35},
-    { apples: 23, oranges: 17, grapes: 43}
-];
+function init() {
+    var dataset = [ // Dataset for the stacked graph
+        { apples: 5, oranges: 10, grapes: 22 },
+        { apples: 4, oranges: 12, grapes: 28 },
+        { apples: 2, oranges: 19, grapes: 32 },
+        { apples: 7, oranges: 23, grapes: 35 },
+        { apples: 23, oranges: 17, grapes: 43 }
+    ];
 
-//set width, height for the chart
-var w = 500;
-var h = 400;
+    var keys = ["apples", "oranges", "grapes"]; // Key each key word and initialize var
 
-//define the color scale
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+    var w = 300;
+    var h = 200;
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 }; // Even padding around the svg
+    
+    var xscale = d3.scaleBand()
+                   .domain(d3.range(dataset.length))
+                   .range([margin.left, w - margin.right]) 
+                   .padding(0.1);
 
-//set up stack method
-var stack = d3.stack()
-                .keys(["apples", "oranges", "grapes"])
-                .order(d3.stackOrderDescending);
+    var yscale = d3.scaleLinear()
+                   .domain([0, d3.max(dataset, d => d.apples + d.oranges + d.grapes)]) // Y axis value based on max of stacked values
+                   .range([h - margin.bottom, margin.top]);
 
-//data, stacked
-var series= stack(dataset);
+    var color = d3.scaleOrdinal(d3.schemeCategory10);  // Color scheme from d3
 
-var xScale = d3.scaleBand()
-                .domain(d3.range(dataset.length))
-                .range([0, w])
-                .paddingInner(0.05);
+    // Stack the data
+    var series = d3.stack()
+                   .keys(keys)(dataset); // Stack keys and dataset 
 
+    var svg = d3.select("#stacked") // Calling it to body in HTML 
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
 
-var yScale = d3.scaleLinear()
-                .domain([0, d3.max(dataset, function(d) {
-                    return d.apples + d.oranges + d.grapes;
-                    })
-                ])
-                .range([h, 0]);
-
-//create svg
-var svg = d3.select("article.content")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
-
-//add group for each row of data
-var groups = svg.selectAll("g")
-                .data(series)
-                .enter()
-                .append("g")
-                .style("fill", function(d, i) {
-                    return color(i);
-                });
-
-
-                //add rect for each data value
-var rects = groups.selectAll("rect")
-                    .data(function(d) { return d; })
+    // Create groups for each series
+    var groups = svg.selectAll("g")
+                    .data(series)
                     .enter()
-                    .append("rect")
-                    .attr("x", function(d, i) {
-                        return xScale(i);
-                    })
-                    .attr("y", function(d, ) {
-                        return yScale(d[1]);
-                    })
-                    .attr("height", function(d) {
-                        return yScale(d[0]) - yScale(d[1]);
-                    })
-                    .attr("width", xScale.bandwidth());
+                    .append("g")
+                    .style("fill", function(d) { return color(d.key); }); // Assign color based on the key (for consistency)
 
-// Legend labels
-var legendLabels = ["Green", "Orange", "Blue"];
+    // Add the rects for each stacked value
+    groups.selectAll("rect")
+          .data(function(d) { return d; })
+          .enter()
+          .append("rect")
+          .attr("x", function(d, i) { return xscale(i); })
+          .attr("y", function(d) { return yscale(d[1]); })
+          .attr("height", function(d) { return yscale(d[0]) - yscale(d[1]); })
+          .attr("width", xscale.bandwidth());
 
-// Create legend
-var legend = svg.selectAll(".legend")
-    .data(legendLabels)
-    .enter()
-    .append("g")
-    .attr("class", "legend")
-    .attr("transform", function(d, i) {
-        return "translate(" + (w + 20) + "," + (i * 20 + 20) + ")"; // Position each legend item
-    });
+    // Add dots for legend
+    svg.selectAll("mydots")
+       .data(keys)
+       .enter()
+       .append("circle")
+       .attr("cx", 20) // Position of dots horizontally
+       .attr("cy", function(d,i){ return 20 + i*25}) // Position of dots vertically
+       .attr("r", 7)
+       .style("fill", function(d){ return color(d)}) // Use the same color scheme based on key
 
-// Append circles to the legend
-legend.append("circle")
-    .attr("cx", 0)
-    .attr("cy", 0)
-    .attr("r", 7)
-    .style("fill", function(d, i) {
-        return colors(["apples", "oranges", "grapes"][i]);
-    });
+    // Add labels for the legend
+    svg.selectAll("mylabels")
+       .data(keys)
+       .enter()
+       .append("text")
+       .attr("x", 30)
+       .attr("y", function(d,i){ return 20 + i*25}) // Position of labels vertically
+       .style("fill", function(d){ return color(d)}) // Use the same color scheme based on key
+       .text(function(d){ return d})
+       .attr("text-anchor", "left")
+       .style("alignment-baseline", "middle");
+}
 
-// Append labels to the legend
-legend.append("text")
-    .attr("x", 15)
-    .attr("y", 4)
-    .text(function(d) {
-        return d;
-    })
-    .style("font-size", "12px")
-    .attr("alignment-baseline", "middle");
+window.onload = init;
