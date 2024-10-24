dcc323e8f76e2de8416c0f35f7d68e5b27de3a44
cremyk <yingkit0212@gmail.com>
2024-10-24 12:32:34 +0800

    update

--- Lab7/task7.3.js
+++ Lab7/task7.3.js
@@ -48,6 +48,15 @@ var svg = d3.select("article.content")
             .attr("width", w)
             .attr("height", h);
 
+// select the svg area
+var svg = d3.select("#legend")
+
+// Handmade legend
+svg.append("circle").attr("cx",200).attr("cy",130).attr("r", 6).style("fill", "#69b3a2")
+svg.append("circle").attr("cx",200).attr("cy",160).attr("r", 6).style("fill", "#404080")
+svg.append("text").attr("x", 220).attr("y", 130).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 220).attr("y", 160).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
+
 //add group for each row of data
 var groups = svg.selectAll("g")
                 .data(series)
@@ -74,26 +83,4 @@ var rects = groups.selectAll("rect")
                     })
                     .attr("width", xScale.bandwidth());
 
-//add one dot in the legend for each name
-var size = 20
-svg.selectAll("mydots")
-.data(keys)
-.enter()
-.append("rect")
-  .attr("x", 100)
-  .attr("y", function(d,i){ return 100 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
-  .attr("width", size)
-  .attr("height", size)
-  .style("fill", function(d){ return color(d)})
 
-// Add one dot in the legend for each name.
-SVG.selectAll("mylabels")
-.data(keys)
-.enter()
-.append("text")
-  .attr("x", 100 + size*1.2)
-  .attr("y", function(d,i){ return 100 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
-  .style("fill", function(d){ return color(d)})
-  .text(function(d){ return d})
-  .attr("text-anchor", "left")
-  .style("alignment-baseline", "middle")
